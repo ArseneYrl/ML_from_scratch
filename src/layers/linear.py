1c98d9ef98fa7a5024f7cd32fe7c61bbd6291bba
ArseneYerle <arseneyerle@MacBook-Pro-de-Arsene.local>
2025-10-05 23:59:50 +0200

    wip: Linear layer implemented

---
+++ src/layers/linear.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Linear:
+    def __init__(self,input_size, output_size):
+        self.input_size = input_size
+        self.output_size = output_size
+
+        self.W = np.random.normal(0,)
