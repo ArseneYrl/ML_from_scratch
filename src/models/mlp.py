0010cc1d9bdd89bb9a3b90b274f003b35793c703
ArseneYerle <arsene.yerle@proton.me>
2025-10-08 13:53:33 +0200

    feature: added forward and backward prop to the MLP

---
+++ src/models/mlp.py
@@ -0,0 +1,20 @@
+import numpy as np
+from src.layers.linear import Linear
+from src.layers.activation import ReLU, Tanh
+
+class MLP:
+    def __init__(self):
+        self.layers=[]
+
+    def add_layer(self,layer):
+        self.layers.append(layer)
+
+    def feedforward(self, X):
+        for layer in self.layers:
+            X = layer.forward(X)
+        return X
+    
+    def backward(self,err):
+        for layer in reversed(self.layers):
+            err = layer.backward(err)
+
