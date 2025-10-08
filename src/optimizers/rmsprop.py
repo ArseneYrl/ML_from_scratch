f2ef6bf912b4abfcc01e583a4310dbd47cf4489e
ArseneYerle <arsene.yerle@proton.me>
2025-10-08 20:20:40 +0200

    added RMSprop optimizer
    git add src/optimizers/rmsprop.py

---
+++ src/optimizers/rmsprop.py
@@ -0,0 +1,15 @@
+import numpy as np
+from .base import Optimizer
+
+class RMSprop(Optimizer):
+    def __init__(self, parameters, lr, gamma):
+        super().__init__(parameters, lr)
+        self.gamma=gamma
+        self.gt = [np.zeros_like(w.data) for w in self.parameters]  
+
+
+    def step(self):
+        for i, param in enumerate(self.parameters):
+            if param.grad is not None:
+                self.gt[i] = self.gamma * self.gt[i] + (1-self.gamma) * (param.grad**2)
+                param.data -= self.lr * param.grad/(np.sqrt(self.gt[i])+1e-8)
