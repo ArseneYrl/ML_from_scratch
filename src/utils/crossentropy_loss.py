908db4ea2807c84fb2a3ffaba947cd4549c779be
ArseneYerle <arsene.yerle@proton.me>
2025-10-08 16:40:53 +0200

    correction of the loss

--- src/utils/crossentropy_loss.py
+++ src/utils/crossentropy_loss.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def crossentropy_loss(ynew, ytrue):
     exp = np.exp(ynew - np.max(ynew, axis=1, keepdims=True))
-    softmax = exp / np.sum(ynew, axis = 1, keepdims=True)
+    softmax = exp / np.sum(exp, axis = 1, keepdims=True)
 
     loss = -np.mean(np.sum(ytrue * np.log(softmax + 1e-8), axis=1))
 
