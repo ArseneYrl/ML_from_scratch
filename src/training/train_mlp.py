ccbeb79f2dc64737682b256499514fb35e329b24
ArseneYerle <arsene.yerle@proton.me>
2025-10-09 19:20:02 +0200

    feature: replaced training function with a class and added accuracy, and validation metrics tracking

--- src/training/train_mlp.py
+++ src/training/train_mlp.py
@@ -1,5 +1,9 @@
 import numpy as np
 from src.utils.crossentropy_loss import crossentropy_loss
+from src.utils.accuracy import accuracy
+import time
+import os
+
 
 def train_mlp(model, optimizer, X, y , epochs, batch_size):
     loss = []
@@ -36,4 +40,70 @@ def train_mlp(model, optimizer, X, y , epochs, batch_size):
         if epoch % 10 == 0:
             print(f"Epoch {epoch}, Loss: {avg_loss}")
 
+
     return loss
+
+
+class Trainer:
+    def __init__(self, model, optimizer, seed = 42):
+        self.model = model
+        self.optimizer = optimizer
+        self.seed = seed
+    
+    def setup_seed(self):
+        np.random.seed(self.seed)
+
+    def train_epoch(self, X, y, batch_size, m):
+
+        indices = np.random.permutation(m)  # Mini-Batch SGD
+        
+        X_shuffled = X[indices]
+        y_shuffled = y[indices]
+
+        for i in range(0, m, batch_size):
+            X_batch = X_shuffled[i:i + batch_size]
+            y_batch = y_shuffled[i:i + batch_size]
+
+            y_new = self.model.feedforward(X_batch)
+
+            _, err = crossentropy_loss(y_new,y_batch)
+
+            self.model.backward(err)
+
+            self.optimizer.step()
+            self.optimizer.zero_grad() 
+
+    def train(self, X_train, y_train, X_val = None, y_val = None, epochs=50, batch_size=32):
+        self.setup_seed()
+        m = len(X_train)
+        history = {'train_loss': [], 'train_acc': [], 'val_loss': [], 'val_acc': []}
+        for epoch in range(epochs):
+
+            self.train_epoch(X_train, y_train, batch_size, m)
+
+            y_pred_train = self.model.feedforward(X_train)
+
+            train_acc = accuracy(y_train, y_pred_train)
+            train_loss, _ = crossentropy_loss(y_train, y_pred_train)
+
+            history['train_loss'].append(train_loss)
+            history['train_acc'].append(train_acc)
+
+            if epoch % 10 == 0:
+                print(f"Epoch {epoch}, Train Loss: {train_loss}, Train Accuracy:{train_acc}")
+
+            if X_val is not None and y_val is not None:
+                y_pred_val = self.model.feedforward(X_val)
+
+                val_acc = accuracy(y_val, y_pred_val)
+                val_loss, _ = crossentropy_loss(y_val, y_pred_val)
+
+                history['val_loss'].append(val_loss)
+                history['val_acc'].append(val_acc)
+
+        return history
+
+            
+
+        
+    
