c3e53f753eab36296fd6039c0a667f9cb4af012e
ArseneYerle <arsene.yerle@proton.me>
2025-10-09 17:30:00 +0200

    added all the activation functions

--- src/layers/activation.py
+++ src/layers/activation.py
@@ -17,3 +17,36 @@ class Tanh: #tanh activation
     def backward(self, error):
         dTanh = 1 - np.tanh(self.z)**2
         return error * dTanh
+    
+class Sigmoid:
+    def forward(self, z):
+        self.z = z
+        return 1/(1-np.exp(-z))
+    
+    def backward(self, error):
+        exp = np.exp(-self.z)
+        dSig = -exp/((1-exp)**2)
+        return error * dSig
+    
+class Leaky_ReLU:
+    def forward(self, z):
+        self.z = z
+        self.cond = (self.z > 0)
+        return np.where(self.cond,0.1*self.z, self.z)
+    
+    def backward(self, error):
+        dLRLU = np.where(self.cond,0.1,1)
+        return error * dLRLU
+    
+class ELU:
+    def forward(self, z):
+        self.z = z
+        self.cond = (self.z > 0)
+        self.exp = np.exp(self.z)
+        return np.where(self.cond, self.exp-1, self.z)
+    
+    def backward(self, error):
+        dELU = np.where(self.cond, self.exp, 1)
+        return error * dELU
+
+
