3f3f5338b4643032f886ed84b9f691b10472d747
ArseneYerle <arsene.yerle@proton.me>
2025-10-13 15:40:13 +0200

    Feature: Implemented early stopping in the trainer

--- src/training/train_mlp.py
+++ src/training/train_mlp.py
@@ -14,6 +14,7 @@ class Trainer:
         np.random.seed(self.seed)
 
     def train_epoch(self, X, y, batch_size, m):
+        #What is done during 1 epoch
 
         indices = np.random.permutation(m)  # Mini-Batch SGD
         
@@ -34,11 +35,19 @@ class Trainer:
             self.optimizer.step()
             self.optimizer.zero_grad() 
 
-    def train(self, X_train, y_train, X_val = None, y_val = None, epochs=50, batch_size=32):
+    def train(self, X_train, y_train, X_val = None, y_val = None, epochs=50, batch_size=32, patience = 5):
+
         self.setup_seed()
         m = len(X_train)
+
         history = {'train_loss': [], 'train_acc': [], 'val_loss': [], 'val_acc': []}
         start_time = time.time()
+
+        if X_val is not None and y_val is not None:
+                y_pred_val = self.model.feedforward(X_val)
+                min_val_loss, _ = crossentropy_loss(y_pred_val, y_val)
+                patience_count = 0
+
         for epoch in range(epochs):
 
             y_pred_train = self.model.feedforward(X_train)
@@ -63,58 +72,27 @@ class Trainer:
                 if epoch % 10 == 0:
                     print(f"Validation Loss: {val_loss}, Validation Accuracy:{val_acc}")
 
+                if val_loss < min_val_loss - 1e-6:  # Amélioration significative
+                    min_val_loss = val_loss
+                    patience_count = 0
+                    best_epoch = epoch
+
+                else: 
+                    patience_count +=1
+                    if patience_count >= patience:
+                        print(f"Early stopping triggered after {epoch+1} epochs")
+                        print(f"Best validation loss: {min_val_loss:.4f} at epoch {best_epoch}")
+                        break
+
 
             self.train_epoch(X_train, y_train, batch_size, m)
 
  
         total_time = time.time() - start_time
-        print("Time:",total_time)
-
+        print(f"Training completed in {total_time:.2f}s")
         return history
     
     def fit(self, X_test, y_test):
         y_pred = self.model.feedforward(X_test)
         return accuracy(y_test, y_pred) 
-        
-
-
-    
-
-'''def train_mlp(model, optimizer, X, y , epochs, batch_size):
-    loss = []
-    m = len(X)
-    y_initial = model.feedforward(X)
-    initial_loss, _ = crossentropy_loss(y_initial,y)
-    loss.append(initial_loss)
-    print("Initial loss:", loss[0])
-    for epoch in range(epochs):
-        epoch_loss = 0
-        num_batches = 0
-        indices = np.random.permutation(m)  # Mini-Batch SGD
-        
-        X_shuffled = X[indices]
-        y_shuffled = y[indices]
-
-        for i in range(0, m, batch_size):
-            X_batch = X_shuffled[i:i + batch_size]
-            y_batch = y_shuffled[i:i + batch_size]
-
-            y_new = model.feedforward(X_batch)
-
-            batch_loss, err = crossentropy_loss(y_new,y_batch)
-            epoch_loss += batch_loss
-            num_batches += 1
-
-            model.backward(err)
-
-            optimizer.step()
-            optimizer.zero_grad() 
-
-        avg_loss = epoch_loss / num_batches
-        loss.append(avg_loss)
-        if epoch % 10 == 0:
-            print(f"Epoch {epoch}, Loss: {avg_loss}")
-
-    return loss'''
-
 
