11bf11d7c133bda137fae1e95ba8076d8e001c28
ArseneYerle <arsene.yerle@proton.me>
2025-11-05 19:20:55 +0100

    WIP: Added backprop of the maxpool layer + minor improvements

--- src/utils/center.py
+++ src/utils/center.py
@@ -1,13 +1,28 @@
 import numpy as np
 
-def center(Input,F, S):
-    #Input will be of shape (W, H, D)
-    centers = []
-    rad = F // 2
-    for i in range(rad,Input.shape[1]-rad,S):
+class Patches:
+    def __init__(self, filter_size, stride):
+        self.stride = stride
+        self.filter_size = filter_size
+        
+    def separate(self, Input):
+        self.X=Input
+        #Input will be of shape (W, H, D)
+        centers = []
+        for i in range(0,Input.shape[1]-self.filter_size+1,self.stride):
+            for j in range(0,Input.shape[2]-self.filter_size+1, self.stride):
 
-        for j in range(rad,Input.shape[2]-rad,S):
+                C=Input[:, i:i+self.filter_size, j:j+self.filter_size]
+                centers.append(C)
+        return centers
+    
+    def reconstruct_grad(self, centers):
+        output = np.zeros(self.X.shape)
+        count = 0
+        for i in range(0,output.shape[1]-self.filter_size + 1,self.stride):
 
-            C=Input[:, i-rad:i+rad+1, j-rad:j+rad+1]
-            centers.append(C)
-    return centers
+            for j in range(0,output.shape[2]-self.filter_size + 1,self.stride):
+
+                output[:, i:i+self.filter_size, j:j+self.filter_size]+=centers[count]
+                count += 1
+        return output
