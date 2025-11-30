eddc6cbb68423cd6790ea47f3ccad261230f467a
ArseneYerle <arsene.yerle@proton.me>
2025-11-30 18:34:04 +0100

    Deleted CNN architecture: Focused too much on the maths, I understand the architecture

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -6,18 +6,21 @@ from src.layers.activation import ReLU, Tanh
 class MLP:
     def __init__(self):
         self.layers=[]
-
+        
     def add_layer(self,layer):
+        #Add a layer to the network
         self.layers.append(layer)
 
     def feedforward(self, X):
+        #Feedforward through all the layers
         for layer in self.layers:
             X = layer.forward(X)
         return X
     
     def backward(self,err):
+        #Backprop through all the layers
         for layer in reversed(self.layers):
-            err = layer.backward(err)
+            err = layer.backward(err)   
 
     def parameters(self):
         params = []
